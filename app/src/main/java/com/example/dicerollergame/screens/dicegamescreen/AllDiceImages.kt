c0899a1a2499776cc484fdefabc3f1e69548c6aa
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-12-25 22:48:04 +0530

    now I implemented the logic of rolling of dice

---
+++ app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/AllDiceImages.kt
@@ -0,0 +1,2 @@
+package com.example.dicerollergame.screens.dicegamescreen
+
--- app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameScreen.kt
+++ app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameScreen.kt
@@ -11,11 +11,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,6 +40,8 @@ fun DiceGameScreen() {
     Scaffold(
         topBar = { DiceGameTopBar() }
     ) { innerPadding ->
+        var player1Score by remember { mutableStateOf(0) }
+        var player2Score by remember { mutableStateOf(0) }
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -59,19 +68,48 @@ fun DiceGameScreen() {
                     horizontalArrangement = Arrangement.SpaceEvenly
                 ) {
                     Text(
-                        "Player01's Score: 0",
+                        "Player01's Score: $player1Score",
                         color = Color.White,
                         fontSize = 16.sp
                     )
                     Text(
-                        "Player02's Score: 0",
+                        "Player02's Score: $player2Score",
                         color = Color.White,
                         fontSize = 16.sp
                     )
                 }
             }
-            Card() {
-                Image(painter = painterResource(R.drawable.))
+            Spacer(modifier = Modifier.height(100.dp))
+            Card(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 24.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(painter = painterResource(R.drawable.dice_01), contentDescription = null)
+                }
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Button(
+                    onClick = {player1Score++}, colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black
+                    ), modifier = Modifier.weight(1f)
+                ) {
+                    Text("P1: Roll Dice")
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(
+                    onClick = {player2Score++}, colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Black
+                    ), modifier = Modifier.weight(1f)
+                ) {
+                    Text("P2: Roll Dice")
+                }
             }
         }
     }
