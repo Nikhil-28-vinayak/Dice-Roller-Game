860441185b2e0774e83068f854bb000f7d310090
Nikhil-28-vinayak <vinayaknikhil2224@gmail.com>
2025-12-25 18:41:52 +0530

    Creating the top app bar and now added in the Dice Game screen

--- app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameScreen.kt
+++ app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameScreen.kt
@@ -1,18 +1,78 @@
 package com.example.dicerollergame.screens.dicegamescreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.dicerollergame.R
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DiceGameScreen(){
+fun DiceGameScreen() {
     Scaffold(
-        topBar = {DiceGameTopBar()}
-    ) {innerPadding->
-        Column(modifier = Modifier.padding(innerPadding)) {  }
+        topBar = { DiceGameTopBar() }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Let's Play",
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Black,
+                fontFamily = FontFamily.Monospace
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.Black, RoundedCornerShape(8.dp))
+                    .height(50.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        "Player01's Score: 0",
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                    Text(
+                        "Player02's Score: 0",
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                }
+            }
+            Card() {
+                Image(painter = painterResource(R.drawable.))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameTopBar.kt
+++ app/src/main/java/com/example/dicerollergame/screens/dicegamescreen/DiceGameTopBar.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -17,7 +19,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun DiceGameTopBar() {
     TopAppBar(
-        title = { Text("Dice Rolling Game") },
+        title = { Text("Dice Rolling Game",
+            fontWeight = FontWeight.SemiBold) },
         actions = {
             Button(
                 onClick = {},
